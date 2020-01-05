c3081717c023deec672a5c7f5097f152288e370f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-04 22:45:19 -0600

    Create download.sh

---
+++ bin/download.sh
@@ -0,0 +1,8 @@
+wget http://www.bio-bigdata.com/lnc2cancer/download/all_lnc_cancer_associations.txt
+wget http://www.bio-bigdata.com/lnc2cancer/download/Circulating_lnc_cancer_associations.txt
+wget http://www.bio-bigdata.com/lnc2cancer/download/Prognostic_lnc_cancer_associations.txt
+wget http://www.bio-bigdata.com/lnc2cancer/download/Drug-resistant_lnc_cancer_associations.txt
+wget http://www.bio-bigdata.com/lnc2cancer/download/LncRNAs%20regulated%20by%20TF%20in%20cancers.txt
+wget http://www.bio-bigdata.com/lnc2cancer/download/LncRNAs%20regulated%20by%20miRNA%20in%20cancers.txt
+wget http://www.bio-bigdata.com/lnc2cancer/download/LncRNAs%20regulated%20by%20variant%20in%20cancers.txt
+wget http://www.bio-bigdata.com/lnc2cancer/download/LncRNAs%20regulated%20by%20methylation%20in%20cancers.txt
